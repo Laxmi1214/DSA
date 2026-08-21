13890310680d7567e7874a1fb76fb978df15d770
Lakshmi Priya A <166808840+Laxmi1214@users.noreply.github.com>
2026-08-21 16:30:41 +0530

    Time: 11 ms (77.36%) | Memory: 19.3 MB (76.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if (sorted(s) == sorted(t)):
-            return True
-        else:
+        if len(s) != len(t):
             return False
+        
+        hashh = {}
+        for letter in s:
+            if letter in hashh:
+                hashh[letter] += 1
+            else:
+                hashh[letter] = 1
+        
+        for j in t:
+            if j not in hashh:
+                return False
+            
+            hashh[j] -= 1
+
+            if hashh[j] < 0:
+                return False
+
+        return True
